5a07a38559342ae1865d72d62a3980ffd222c025
Guilherme Satoshi Drefahl Hirata <102560162+guilhermehirata@users.noreply.github.com>
2022-07-04 19:57:28 -0300

    Add files via upload

---
+++ 2_bim/guilherme-hirata/src/js/data.js
@@ -0,0 +1,26 @@
+console.log('Bem-Vindo!')
+let date = new Date()
+
+
+
+let social = [{
+  name: 'github',
+  link: 'https://github.com/guilhermehirata',
+},
+{
+  name: 'linkedin',
+  link: 'https://linkedin.com/in/guilhermehirata',
+},
+{
+  name: 'whatsapp',
+  link: 'https://api.whatsapp.com/',
+},
+{
+  name: 'gmail',
+  link: 'mailto:guilherme.hirata12@gmail.com',
+},
+{
+  name: 'curriculum',
+  link: '',
+}
+]
